200c348edf613a7fbafcdd68def667e8404f5a93
Victor Bravim <victorrafaelbravim@gmail.com>
2024-03-05 22:50:44 -0300

    Mudança

--- 2Dia/script.js
+++ 2Dia/script.js
@@ -1,22 +1,21 @@
-function updateClock() {
-    const now = new Date();
+document.addEventListener('DOMContentLoaded', function() {
+    const inc = 1000;
 
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    function clock() {
+        const date = new Date();
 
-    const hourHand = document.querySelector('.hour-hand');
-    const minHand = document.querySelector('.min-hand');
-    const secondHand = document.querySelector('.second-hand');
+        const hours = ((date.getHours() + 11) % 12 + 1);
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        
+        const hour = hours * 30;
+        const minute = minutes * 6;
+        const second = seconds * 6;
+        
+        document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
+        document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
+        document.querySelector('.second').style.transform = `rotate(${second}deg)`;
+    }
 
-    const hourDeg = (hours % 12 + minutes / 60) * 360 / 12;
-    const minDeg = (minutes + seconds / 60) * 360 / 60;
-    const secondDeg = seconds * 360 / 60;
-
-    hourHand.style.transform = `rotate(${hourDeg}deg)`;
-    minHand.style.transform = `rotate(${minDeg}deg)`;
-    secondHand.style.transform = `rotate(${secondDeg}deg)`;
-}
-
-setInterval(updateClock, 1000); // Atualiza o relógio a cada segundo
-updateClock(); // Chama a função uma vez para iniciar o relógio imediatamente
+    setInterval(clock, inc);
+});
