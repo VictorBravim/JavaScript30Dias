6fe112ad9eeb7fde1185e81489c9140de0db35c6
Victor Bravim <victorrafaelbravim@gmail.com>
2024-03-07 17:46:34 -0300

    Mudança

--- 4Dia/script.js
+++ 4Dia/script.js
@@ -1,3 +1,4 @@
+// Seleção de elementos DOM
 var $keyboardWrapper = $('.virtual-keyboard'),
     $key = $keyboardWrapper.find("input"),
     $key_delete = $('.delete'),
@@ -7,13 +8,14 @@ var $keyboardWrapper = $('.virtual-keyboard'),
     actionKeys = $(".delete,.shift")
 activeShiftClass = "shift-activated";
 
-// handle keystrokes
+// Função para lidar com as teclas do teclado virtual
 function _keystroke(keyCase) {
 
+    // Evento de clique nas teclas (exceto as teclas de ação)
     $key.not(actionKeys).on('click', function (e) {
         e.preventDefault();
 
-        // check for shift key for upper
+        // Verifica se a tecla Shift está ativada
         if ($key_shift.hasClass(activeShiftClass)) {
             keyCase = 'upper';
             $key_shift.removeClass(activeShiftClass);
@@ -21,45 +23,49 @@ function _keystroke(keyCase) {
             keyCase = 'lower';
         }
 
-        // handle case
+        // Determina o valor da tecla com base no caso (maiúsculo ou minúsculo)
         if (keyCase == 'upper') {
             var keyValue = $(this).val().toUpperCase();
         } else {
             var keyValue = $(this).val().toLowerCase();
         }
 
-        // grab current value
+        // Obtém o valor atual do campo de saída, concatena a tecla pressionada e atualiza o campo
         var output = $('.output input').val();
         $outputField.val(output + keyValue);
         getCurrentVal();
         focusOutputField();
     });
 
-} // keystroke
+}
 
-// delete
+// Evento de clique na tecla de exclusão
 $key_delete.on('click', function (e) {
     e.preventDefault();
+
+    // Remove o último caractere do valor atual do campo de saída
     $outputField.val($currentValue.substr(0, $currentValue.length - 1));
     getCurrentVal();
     focusOutputField();
 });
 
-// shift
+// Evento de clique na tecla Shift
 $key_shift.on('click', function (e) {
     e.preventDefault();
+
+    // Alternância da classe 'shift-activated' na tecla Shift
     $(this).toggleClass(activeShiftClass);
 });
 
-// grab current value of typed text
+// Função para obter o valor atual do campo de saída
 function getCurrentVal() {
     $currentValue = $outputField.val();
 }
 
-// focus for cursor hack
+// Função para focar no campo de saída
 function focusOutputField() {
     $outputField.focus();
 }
 
-_keystroke("lower"); // init keystrokes
-
+// Inicia o teclado virtual com as teclas em caixa baixa
+_keystroke("lower");
