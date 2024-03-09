a9bf34afd8f8c80a548ca45d7fdf93fc71f30d04
Victor Bravim <victorrafaelbravim@gmail.com>
2024-03-09 17:46:44 -0300

    Mudança

--- 6Dia/script.js
+++ 6Dia/script.js
@@ -8,21 +8,21 @@ const main = document.getElementById('main');
 
 // Function that retrieves user data from the API
 async function getUser(username) {
-  try {
-    const { data } = await axios(API_URL + username);
-    createUserCard(data);
-    getRepos(username);
-  } catch (err) {
-    // If user is not found, create an error card
-    if (err.response.status == 404) {
-      createErrorCard('No User found');
+    try {
+        const { data } = await axios(API_URL + username);
+        createUserCard(data);
+        getRepos(username);
+    } catch (err) {
+        // If user is not found, create an error card
+        if (err.response.status == 404) {
+            createErrorCard('No User found');
+        }
     }
-  }
 }
 
 // Function that creates a card with user information
 function createUserCard(user) {
-  main.innerHTML = `
+    main.innerHTML = `
     <div class="card">
       <div class="load-anim"><img class="avatar" src="${user.avatar_url}" alt="${user.name}" /></div>
       <div class="user-info">
@@ -40,7 +40,7 @@ function createUserCard(user) {
 
 // Function that creates an error card
 function createErrorCard(message) {
-  main.innerHTML = `
+    main.innerHTML = `
     <div class="card">
         <h1>${message}</h1>
     </div>
@@ -49,38 +49,38 @@ function createErrorCard(message) {
 
 // Function that retrieves repository data from the API
 async function getRepos(username) {
-  try {
-    const { data } = await axios(API_URL + username + '/repos?sort=created');
-    addReposToCard(data);
-  } catch (err) {
-    createErrorCard('Problem fetching repos');
-  }
+    try {
+        const { data } = await axios(API_URL + username + '/repos?sort=created');
+        addReposToCard(data);
+    } catch (err) {
+        createErrorCard('Problem fetching repos');
+    }
 }
 
 // Function that adds a user's repositories to their card
 function addReposToCard(repos) {
-  const reposEl = document.getElementById('repos');
+    const reposEl = document.getElementById('repos');
 
-  repos.slice(0, 5).forEach((repo) => {
-    const repoLink = document.createElement('a');
-    repoLink.classList.add('repo', 'load-anim', 'load-anim-text');
-    repoLink.href = repo.html_url;
-    repoLink.target = '_blank';
-    repoLink.innerHTML = repo.name;
+    repos.slice(0, 5).forEach((repo) => {
+        const repoLink = document.createElement('a');
+        repoLink.classList.add('repo', 'load-anim', 'load-anim-text');
+        repoLink.href = repo.html_url;
+        repoLink.target = '_blank';
+        repoLink.innerHTML = repo.name;
 
-    reposEl.appendChild(repoLink);
-  });
+        reposEl.appendChild(repoLink);
+    });
 }
 
 // Event listener for when the form is submitted
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const user = search.value;
+    const user = search.value;
 
-  // If a username was entered, retrieve user data and clear the search field
-  if (user) {
-    getUser(user);
-    search.value = '';
-  }
+    // If a username was entered, retrieve user data and clear the search field
+    if (user) {
+        getUser(user);
+        search.value = '';
+    }
 });
